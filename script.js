8856ac647142f54a082ce4bc767e496c3b87749d
Prathik Mishra <126965610+Prathik-mishra@users.noreply.github.com>
2024-09-18 16:34:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+document.getElementById("from1");
+	let fname1=from.get('fname');
+	let fname2=from.get('lname');
 
+	alert(fname1+" "+fname2)
 }
